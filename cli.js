3b9750c2fc8a9763942897e1b8c56d0f847e3b36
TunG <mt@live.hk>
2016-12-10 01:33:50 +0800

    Fix cli arguments problem

--- cli.js
+++ cli.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-var electron = require('electron');
-var proc = require('child_process');
+var electron = require('electron'),
+    proc = require('child_process');
 
 var args = [`${__dirname}/index.js`];
-if (process.argv.length > 2) args.push(process.argv.slice(2));
+if (process.argv.length > 2) args = args.concat(process.argv.slice(2));
 
 var child = proc.spawn(electron, args, { stdio: 'inherit' });
 child.on('close', function (code) {
